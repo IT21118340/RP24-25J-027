025d842f97f224c69352a5bde9c12bc48e4ba844
IT21118340 <it21118340@my.sliit.lk>
2025-03-16 23:08:48 +0530

    Update main.jsx

--- Web App/src/main.jsx
+++ Web App/src/main.jsx
@@ -1,12 +1,13 @@
 import "./index.css"; // This should be the first line
-import { React, StrictMode } from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import { React } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.jsx";
 import AppFooter from "./components/AppFooter.jsx";
 import AppHeader from "./components/AppHeader.jsx";
-import Skills from "./pages/Skills.jsx";
 import Quizes from "./pages/AllQuizes.jsx";
+import Skills from "./pages/Skills.jsx";
+import NewQuiz from "./pages/NewQuiz.jsx";
 /*
 import FinalTest from "./pages/FinalTest.jsx";
 import Quiz from "./pages/QuizPage.jsx";
@@ -19,9 +20,11 @@ createRoot(document.getElementById("root")).render(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />} />
-        <Route path="/skills" element={<Skills />} />
-        
+				<Route path="/skills" element={<Skills />} />
+
 				<Route path="/quizes" element={<Quizes />} />
+
+				<Route path="/newquiz" element={<NewQuiz />} />
 				{/* 
 
 				<Route path="/quiz" element={<Quiz />} />
