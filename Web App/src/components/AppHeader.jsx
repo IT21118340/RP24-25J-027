89a42621ec64c36ed4255755de80e4ec66185480
IT21118340 <it21118340@my.sliit.lk>
2025-03-16 23:14:16 +0530

    Update AppHeader.jsx

--- Web App/src/components/AppHeader.jsx
+++ Web App/src/components/AppHeader.jsx
@@ -8,17 +8,57 @@ function AppHeader() {
 				justifyContent: "space-between",
 				alignItems: "center",
 				borderBottom: "1px solid #ddd",
-				marginBottom: "2%",
-				backgroundColor: "black",
+				marginBottom: "1%",
 			}}
 		>
-			<h1
-				className="text-white text-2xl font-[Open_Sans]"
-				color="white"
-				style={{ marginLeft: "1%" }}
+			<div
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
 			>
-				Readify
-			</h1>
+				<img
+					src="src\assets\AppIcon.png"
+					alt="User Profile"
+					style={{
+						height: "100%",
+						maxHeight: "50px",
+						objectFit: "cover",
+						cursor: "pointer",
+					}}
+				/>
+				<p
+					className="text-2xl font-[Open_Sans]"
+					color="white"
+					style={{ marginLeft: "1%" }}
+				>
+					Readify
+				</p>
+			</div>
+
+			<div class="grow"> &nbsp; </div>
+			<div
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					gap: "64px",
+				}}
+			>
+				<button className="text-lg font-[Open_Sans] hover:underline">
+					Home
+				</button>
+				<button className="text-lg font-[Open_Sans]  hover:underline">
+					Learn Strategies
+				</button>
+				<button className="text-lg font-[Open_Sans]  hover:underline">
+					Quizes
+				</button>
+				<button className="text-lg font-[Open_Sans]  hover:underline">
+					Final Exam
+				</button>
+			</div>
 
 			<img
 				className="avatar"
@@ -29,6 +69,7 @@ function AppHeader() {
 					maxHeight: "50px",
 					objectFit: "cover",
 					cursor: "pointer",
+					marginLeft: "5%",
 				}}
 			/>
 		</header>
