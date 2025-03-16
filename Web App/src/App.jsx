aa3c69a9ab7b4d6e577fbf8dced5bde5771c059d
IT21118340 <it21118340@my.sliit.lk>
2025-03-16 23:11:08 +0530

    Update App.jsx

--- Web App/src/App.jsx
+++ Web App/src/App.jsx
@@ -1,38 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router";
 import "./App.css";
 
 function App() {
+	const [showAlert, setShowAlert] = useState(false);
+
 	const cardDetails = [
 		{
 			title: "Reading Comprehension Strategies",
 			description:
 				"Improve your reading comprehension abilities with these expert strategies and techniques. Unlock your potential to analyze complex texts with confidence.",
 			img: "src/assets/Skills.jpg",
+			link: "/skills",
+			status: "active",
 		},
 		{
 			title: "Interactive Quizzes",
 			description:
 				"Test your understanding and track your progress with engaging quizzes. Receive instant and comprehensive feedback and pinpoint areas for improvement.",
 			img: "src/assets/Quizes.jpg",
-		},
-		{
-			title: "Instant Answer Insight",
-			description:
-				"Snap a picture of your written answers, and this module will analyze your work, providing instant, personalized feedback",
-			img: "src/assets/Pastpapers.jpg",
+			link: "/quizes",
+			status: "active",
 		},
 		{
 			title: "Final Exam",
 			description:
 				"Put your skills to the ultimate test with this final exam. Gauge your readiness and solidify your mastery of advanced reading comprehension.",
 			img: "src/assets/FinalExam.jpg",
+			link: "/",
+			status: "inactive",
 		},
 	];
 
+	const handleLockedButtonClick = () => {
+		setShowAlert(true);
+		setTimeout(() => {
+			setShowAlert(false);
+		}, 3000);
+	};
+
 	return (
 		<>
-			<center>
-				<h1 className="text-4xl font-bold underline font-[Open_Sans]">
+			<center style={{ margin: "0.5%" }}>
+				{showAlert && (
+					<div role="alert" className="alert alert-warning">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6 shrink-0 stroke-current"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+							/>
+						</svg>
+						<span>
+							Warning: Final Exam is currently locked! You need to
+							complete at least five quizes to unlock this.
+						</span>
+					</div>
+				)}
+
+				<h1
+					className="text-4xl font-bold underline font-[Open_Sans]"
+					style={{ marginTop: "1%" }}
+				>
 					Welcome to Advanced Reading Comprehension Skill Module!
 				</h1>
 
@@ -55,9 +90,20 @@ function App() {
 								<h2 className="card-title">{card.title}</h2>
 								<p>{card.description}</p>
 								<div className="card-actions">
-									<button className="btn btn-primary">
-										Learn More
-									</button>
+									{card.status === "active" ? (
+										<NavLink to={card.link} end>
+											<button className="btn btn-primary">
+												Try Now
+											</button>
+										</NavLink>
+									) : (
+										<button
+											className="btn btn-neutral"
+											onClick={handleLockedButtonClick}
+										>
+											Locked
+										</button>
+									)}
 								</div>
 							</div>
 						</div>
