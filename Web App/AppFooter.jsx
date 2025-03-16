d194f2c3adcd0f8e13a0c63bb0f008c5f5051f58
IT21118340 <it21118340@my.sliit.lk>
2025-03-16 17:28:05 +0530

    Create AppFooter.jsx

---
+++ Web App/AppFooter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AppFooter() {
+	return (
+		<footer
+			style={{
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+				position: "fixed",
+				bottom: 0,
+				width: "100%",
+				backgroundColor: "black",
+				padding: "1%"
+			}}
+		>
+		</footer>
+	);
+}
+
+export default AppFooter;
