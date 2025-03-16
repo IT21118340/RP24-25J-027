7b189f35230d24d735e5ff410ec3fb32f90ea560
IT21118340 <it21118340@my.sliit.lk>
2025-03-16 17:25:33 +0530

    Create AllQuizes.jsx

---
+++ Web App/pages/AllQuizes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Quizes() {
+	return (
+		<center>
+			<h2 className="text-2xl font-bold font-[Open_Sans]">Quizes!!</h2>
+
+			<br />
+
+			<div className="flex flex-wrap gap-8 justify-center"></div>
+		</center>
+	);
+}
+
+export default Quizes;
